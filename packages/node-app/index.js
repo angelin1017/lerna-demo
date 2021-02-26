ea935dafd1259b561a15cfec2e8c03aad358f74c
fengsi <fengsi.fs@alibaba-inc.com>
2021-02-26 16:50:42 +0800

    feat: 删除react项目

--- packages/node-app/index.js
+++ packages/node-app/index.js
@@ -1,5 +1,8 @@
 const esModule = require('es-module').default;
 const commonJS = require('commonjs');
+let formateTime = require('npm-publish-module1-fs/index');
+const time = formateTime(new Date())
+console.log('fengsi', time)
 
 (function() {
   console.log('This is core function.');
